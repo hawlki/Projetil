66fe316a728279d66e9d56bd7d39db19e4cf9fda
israel lopes <israel.lopes2710@gmail.com>
2021-03-24 18:16:20 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace projetil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Beep();
+
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Etec Adolpho Berezin");
+            Console.WriteLine("Feito por Israel Camilo Lopes");
+            Console.WriteLine("1I3");
+            Console.WriteLine("-----------------------------");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Escreva um valor para a velocidade em (m/s): ");
+            String velocidade = Console.ReadLine();
+            Console.Write("Escreva o valor de um ângulo em graus: ");
+            String angulog = Console.ReadLine(); //angulog: ângulo em graus
+            Console.WriteLine();
+
+            const double g = 9.80665;
+            Double v = Convert.ToDouble(velocidade);
+            Double a = Convert.ToDouble(angulog);
+            Double ar = a * (Math.PI / 180); //ar: ângulo em radianos
+            Double alcance = (Math.Pow(v, 2) * Math.Sin(2 * ar)) / g;
+            Double altura = Math.Pow((v * Math.Sin(ar)), 2) / (2 * g);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"O alcance é {alcance} m");
+            Console.WriteLine($"A altura é {altura} m");
+            Console.WriteLine();
+
+            Console.ResetColor();
+            Console.WriteLine("Toque em uma tecla para finalizar o programa");
+            Console.ReadKey();
+
+            Console.Clear();
+        }
+    }
+}
